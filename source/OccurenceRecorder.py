01f36cf465b28eeec79efd57608c2b5625d4f269
EduardoEscobar <edu.escobar@hotmail.com>
2016-10-13 00:13:57 +0000

    Compactacao do arquivo final

--- source/OccurenceRecorder.py
+++ source/OccurenceRecorder.py
@@ -1,6 +1,7 @@
 # -- coding: utf-8 --
 from VideoRecorder import VideoRecorder
 from AudioRecorder import AudioRecorder
+from ZipHelper import *
 import threading
 import time
 import os
@@ -10,7 +11,10 @@ import shutil
 class OccurenceRecorder(object):
 
     def __init__(self):
-        self.clean_tmp_folder()
+        self.folder_path = "../tmp"
+        self.zip_file = "file.zip"
+        zipdir(self.folder_path, self. zip_file)
+        #self.clean_tmp_folder()
         self.audioRecorder = AudioRecorder()
         self.videoRecorder = VideoRecorder()
 
@@ -29,11 +33,11 @@ class OccurenceRecorder(object):
             time.sleep(0.1)
         self.audioRecorder.cleanup()
         self.videoRecorder.cleanup()
+        zipdir(self.folder_path, self. zip_file)
 
     def clean_tmp_folder(self):
-        folder = "../tmp"
-        for file in os.listdir(folder):
-            file_path = os.path.join(folder, file)
+        for file in os.listdir(self.folder_path):
+            file_path = os.path.join(self.folder_path, file)
             if os.path.isfile(file_path):
                 os.unlink(file_path)
             elif os.path.isdir(file_path):
